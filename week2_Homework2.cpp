42c1166d4cf5f75fd4cdeb03ccca09b95c500244
PhillzZerox <67993422+PhillzZerox@users.noreply.github.com>
2020-07-17 11:08:00 +0700

    Add files via upload

---
+++ week2_Homework2.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<string>
+#include<math.h>
+using namespace std;
+int main()
+{
+	//************Homework 2***************
+	string firstname, lastname;
+	int salary, sale, percent;
+	float total;
+	cout << " Enter Name   : ";
+	cin >> firstname >> lastname;
+	cout << " Enter salary : ";
+	cin >> salary;
+	cout << " Enter sale   : ";
+	cin >> sale;
+	cout << " Enter Comission Percent : ";
+	cin >> percent;
+
+	//************output****************
+	cout << " Your name = " << firstname <<" "<< lastname << endl;
+
+	//***************สูตร****************
+	total = (sale*percent/100)+salary;
+	cout << " Total Revenue : " << total << " Bath " << endl;
+
+	return(0);
+
+	//********Chayanin chaklang SecB No.21*********
+}
